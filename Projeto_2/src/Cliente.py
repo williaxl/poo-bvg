f883d1ef141e4ca2c341c17268aa776a5d233273
Williaxl <axelw880@gmail.com>
2025-11-13 14:44:15 -0300

    Projeto_3 - William Axel: Estrutura finalizada limpa

--- Projeto_2/src/Cliente.py
+++
@@ -1,8 +0,0 @@
-class Cliente:
-    def __init__(self, nome, cpf):
-        self.__nome = nome
-        self.__cpf = cpf
-    def exibir_informacoes(self):
-        return f"Cliente: {self.__nome} - CPF: {self.__cpf}"
-    def get_cpf(self):
-        return self.__cpf
